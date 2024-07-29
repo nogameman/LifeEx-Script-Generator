73d9ac02b214e376ee2b87e505d49ae7de40add8
Ajavindu Prasad <148918579+nogameman@users.noreply.github.com>
2024-07-29 14:35:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def write_to_files(output_path, patients, folder_image, folder_roi, file_suffix,
         file.write("# LIFEx.Output.Directory=" + output_path.replace('\\', '/') + "\n\n")
         for index, patient_id in enumerate(patients):
             image_path = os.path.join(folder_image, f'{patient_id}{file_suffix}').replace('\\', '/')
-            roi_path = os.path.join(folder_roi, f'binary_{patient_id}_segm.nii.gz').replace('\\', '/')
+            roi_path = os.path.join(folder_roi, f'binary_{patient_id}_roi.nii.gz').replace('\\', '/')
             
             file.write(f'LIFEx.Patient{index}.Series0={image_path}\n')
             file.write(op_template.format(index))
@@ -50,9 +50,20 @@ def main():
     folder_roi = os.getenv('FOLDER_ROI', 'rois')
     output_path = os.getenv('OUTPUT_PATH', 'output/config.txt')
     file_suffix = os.getenv('FILE_SUFFIX', '_image.nii.gz')
-    op_template = 'LIFEx.Patient{}.Series0.Operation0=Texture,true,false,false,1,3d,RelativeMinMax,8.0,0.0\n'
     
-    patients = get_patient_ids(folder_roi, 'binary_', '_segm.nii.gz')
+    # Documentation for op_template:
+    # The op_template variable defines the operations to be performed on the images.
+    # It uses a placeholder for the patient index and can be customized to include various parameters:
+    #   - Operation type: 'Texture' in this case.
+    #   - Boolean flags: For specific operation settings (e.g., true, false, false).
+    #   - Dimension: '3d' for 3D operations.
+    #   - Method: 'RelativeMinMax' for relative min-max normalization.
+    #   - Bin width: The second-to-last parameter (e.g., 8.0).
+    # Adjust the bin width as needed.
+    bin_width = os.getenv('BIN_WIDTH', '8.0')
+    op_template = f'LIFEx.Patient{{}}.Series0.Operation0=Texture,true,false,false,1,3d,RelativeMinMax,{bin_width},0.0\n'
+    
+    patients = get_patient_ids(folder_roi, 'binary_', '_roi.nii.gz')
     
     write_to_files(output_path, patients, folder_image, folder_roi, file_suffix, op_template)
 
